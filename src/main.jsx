ee06ccd5f2eb49f843b59420f13ba622bf81db30
mrraghuvar <www.raghuvarjha@gmail.com>
2024-07-11 00:50:21 +0530

    Describe your changes here

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  BrowserRouter,
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
@@ -55,6 +54,6 @@ const mainPage = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<BrowserRouter router={mainPage} />
-  </React.StrictMode>,
+<RouterProvider router={mainPage} />
+  </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/mrraghuvar.github.io/",
   plugins: [react()],
-  base:"/mrraghuvar/mrraghuvar.github.io/"
 })
