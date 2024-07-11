697a48129f33564c5f42268c82c65726e79ce98c
mrraghuvar <www.raghuvarjha@gmail.com>
2024-07-11 18:35:08 +0530

    Describe your changes here

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Home from '../Home'
-import './index.css'
-import Sllaybus from '../Routes/syllabus';
-import Cho from '../Routes/cho';
-import Impq from '../Routes/impq';
-import Notes from '../Routes/notes';
-import Resource from '../Routes/resource';
-import Updates from '../Routes/updates';
+import Home from './Home';
+import './index.css';
+import Sllaybus from '../src/Routes/syllabus';
+import Cho from '../src/Routes/cho';
+import Impq from '../src/Routes/impq';
+import Notes from '../src/Routes/notes';
+import Resource from '../src/Routes/resource';
+import Updates from '../src/Routes/updates';
 
 const mainPage = createBrowserRouter([
   {
